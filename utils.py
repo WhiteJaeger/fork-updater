3c862da8c3e93a408c42e905fba0fc6d02fd8a56
Andrej Kashchikhin <logerk3@gmail.com>
2021-11-19 08:35:19 +0300

    hardcode, add more forks

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 
 def get_forks() -> Dict[str, str]:
-    forks_data = requests.get('https://api.github.com/repos/fipl-hse/2021-2-level-labs/forks').json()
+    forks_data = requests.get('https://api.github.com/repos/fipl-hse/2021-2-level-labs/forks?per_page=100').json()
     forks = {}
     for fork in forks_data:
         forks[fork['owner']['login']] = fork['html_url']
