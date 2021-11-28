5f698253f7e8390f91efba015624e1dc5fda6bdb
Andrej Kashchikhin <logerk3@gmail.com>
2021-11-28 12:57:13 +0300

    start refactoring update

--- update.sh
+++ update.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-mkdir -m 777 -p /tmp/forkUpdate
+# Arguments
+## 1: tmp folder id
+## 2: fork name
+## 3: fork url
+## 4: upstream url
+## 5: user
 
-pushd /tmp/forkUpdate
+TMP_FOLDER_ID="$1"
+FORK_NAME="$2"
+FORK_URL="$3"
+UPSTREAM_URL="$4"
+GH_USER="$5"
 
-FORK_URL="$1"
-UPSTREAM_URL="$2"
+# Create dir for fork
+mkdir -m 770 -p "/tmp/forkUpdate${TMP_FOLDER_ID}"
+pushd "/tmp/forkUpdate${TMP_FOLDER_ID}"
+
+# Need to remove 'https://'
+FORK_URL="${FORK_URL:8}"
+FORK_URL="https://x-access-token:${AUTH_TOKEN}@${FORK_URL}"
+
+git config --global user.name "${USER}"
+git config --global user.email "${USER}@users.noreply.github.com"
 
 git clone "git@github.com:WhiteJaeger/mock-2021.git" fork
 
